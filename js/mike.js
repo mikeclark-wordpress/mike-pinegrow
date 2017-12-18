cf5ccd998cedb32868d2580123029835e53a7256
mike <mikeclark@mossycity.com>
2017-12-18 11:02:07 -0800

    css changes

--- js/mike.js
+++ js/mike.js
@@ -2,7 +2,13 @@
 this function will add different background color classes to "#wrapper" when the window is scrolled to a certain height. It will also remove the next color class so it reverts to the appropriate color when the user scrolls back up. Could be simplified, but this is easy to understand and alter.
 */
 
+if($('.navbar-toggle').css('display')!='none')
+{
+    $('.menu-color').addClass('colorTwo');  
+}
+
 $(window).scroll(function() {
+  if($('.navbar-toggle').css('display')!='none') return;
   if ($(this).scrollTop() <=50) {
     $('.menu-color').addClass('colorOne')
       .removeClass('colorTwo');
